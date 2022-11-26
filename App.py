aa85435159bc5e8363e4057e7ac052b7da6d2faf
kobe892021 <kobe892021@gmail.com>
2022-11-27 01:36:07 +0800

    新增國軍醫院-台中

--- App.py
+++ App.py
@@ -16,6 +16,7 @@ from fyh import FYH
 from rg import RG
 from everan import EVERAN
 from tafghzb import TAFGHZB
+from tafgh import TAFGH
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
@@ -38,6 +39,7 @@ class MainWindows():
             "中國醫學大學豐原分院" : None,
             "豐原醫院" : None,
             "太平長安醫院" : None,
+            "國軍醫院-中清" : None,
             "國軍醫院-台中" : None,
         }
         self.RunStatus = True
@@ -324,7 +326,8 @@ class MainWindows():
             "中國醫學大學豐原分院" : CMUH(self.browser,self,self.beginPage.get(),self.beginNum.get(),self.endPage.get(),self.endNum.get(),self.outputPath,self.filePath),
             "豐原醫院" : FYH(self.browser,self,self.beginPage.get(),self.beginNum.get(),self.endPage.get(),self.endNum.get(),self.outputPath,self.filePath),
             "太平長安醫院" : EVERAN(self.browser,self,self.beginPage.get(),self.beginNum.get(),self.endPage.get(),self.endNum.get(),self.outputPath,self.filePath),
-            "國軍醫院-台中" : TAFGHZB(self.browser,self,self.beginPage.get(),self.beginNum.get(),self.endPage.get(),self.endNum.get(),self.outputPath,self.filePath),
+            "國軍醫院-中清" : TAFGHZB(self.browser,self,self.beginPage.get(),self.beginNum.get(),self.endPage.get(),self.endNum.get(),self.outputPath,self.filePath),
+            "國軍醫院-台中" : TAFGH(self.browser,self,self.beginPage.get(),self.beginNum.get(),self.endPage.get(),self.endNum.get(),self.outputPath,self.filePath),
         }
         t = threading.Thread(target=self.mainProcess)
         t.start()
--- tafgh.py
+++ tafgh.py
@@ -6,8 +6,8 @@ import ddddocr
 import os
 from PDFReader import PDFReader
 
-# 國軍醫院-中清
-class TAFGHZB():
+# 國軍醫院-台中
+class TAFGH():
     def __init__(self, browser, mainWindowsObj, S_Page:int=1, S_Num:int=1, E_Page:int=1, E_Num:int=5, outputFile:str=None, filePath:str=None) -> None:
         if E_Num == '':
             E_Num = 5
@@ -42,7 +42,7 @@ class TAFGHZB():
                 for persionData in self.Data :
                     print(persionData)
                     if (self.currentNum <= self.EndNum) and (self.currentPage <= self.EndPage) and self.window.RunStatus:
-                        content = "姓名 : " + persionData['Name'] + "\n身分證字號 : " + persionData['ID'] + "\n出生日期 : " + persionData['Born'] + "\n查詢醫院 : 國軍醫院-中清\n當前第" + str(self.currentPage) + "頁，第" + str(self.currentNum) + "筆"
+                        content = "姓名 : " + persionData['Name'] + "\n身分證字號 : " + persionData['ID'] + "\n出生日期 : " + persionData['Born'] + "\n查詢醫院 : 國軍醫院-台中\n當前第" + str(self.currentPage) + "頁，第" + str(self.currentNum) + "筆"
                         self.window.setStatusText(content=content,x=0.3,y=0.75,size=12)
                         self._getReslut(persionData['Name'], persionData['ID'], persionData['Born'].split('/')[0],persionData['Born'].split('/')[1],persionData['Born'].split('/')[2])
                         self._startBrowser(persionData['Name'],persionData['ID'])
@@ -66,7 +66,7 @@ class TAFGHZB():
             # 利用迴圈自動重試
             while True:
                 # 獲取登入網頁回應
-                respone = client.get('https://web-reg-server.803.org.tw/816/WebReg/book_query')
+                respone = client.get('https://web-reg-server.803.org.tw/803/webreg/book_query')
                 soup = BeautifulSoup(respone.content,"html.parser")
                 time.sleep(1)
 
@@ -74,17 +74,17 @@ class TAFGHZB():
                 self.payload['__RequestVerificationToken'] = soup.find('form',{'method':'post'}).find('input',{'name':'__RequestVerificationToken'}).get('value')
                 
                 # 請求驗證碼
-                respone = client.get('https://web-reg-server.803.org.tw/816/captcha-img')
+                respone = client.get('https://web-reg-server.803.org.tw/803/captcha-img')
                 with open('VaildCode.png','wb') as f :
                     f.write(respone.content)
                 self.payload['vcode'] = self._ParseCaptcha()
 
                 # 發送登入請求
-                respone = client.post('https://web-reg-server.803.org.tw/816/WebReg/book_query', headers=self.headers, data=self.payload)
+                respone = client.post('https://web-reg-server.803.org.tw/803/WebReg/book_query', headers=self.headers, data=self.payload)
                 # 檢查是否登入成功(有登入成功此網站會回應302)
                 if(respone.status_code == 302):
                     # 查詢掛號紀錄
-                    respone = client.get('https://web-reg-server.803.org.tw/816/WebReg/book_detail')
+                    respone = client.get('https://web-reg-server.803.org.tw/803/WebReg/book_detail')
                     with open('reslut.html','w', encoding='utf-8') as f :
                         f.write(self._changeHTMLStyle(respone.content))
                     break
@@ -99,12 +99,12 @@ class TAFGHZB():
                     # 有病歷資料的話即為驗證碼輸入錯誤，進行重試
                     self.window.setStatusText(content="驗證碼錯誤，系統正重新查詢",x=0.2,y=0.8,size=20)
                     time.sleep(1)
-                    content = "姓名 : " + name + "\n身分證字號 : " + ID + "\n出生日期 : " + (year + "/" + month + "/" + day) + "\n查詢醫院 : 國軍醫院-中清\n當前第" + str(self.currentPage) + "頁，第" + str(self.currentNum) + "筆"
+                    content = "姓名 : " + name + "\n身分證字號 : " + ID + "\n出生日期 : " + (year + "/" + month + "/" + day) + "\n查詢醫院 : 國軍醫院-台中\n當前第" + str(self.currentPage) + "頁，第" + str(self.currentNum) + "筆"
                     self.window.setStatusText(content=content,x=0.3,y=0.75,size=12)
 
     def _startBrowser(self,name,ID):
         self.browser.get(r'file:///' + os.path.dirname(os.path.abspath(__file__)) + '/reslut.html')
-        if self._Screenshot("我要取消",(name + '_' + ID + '_國軍醫院-中清.png')) :
+        if self._Screenshot("我要取消",(name + '_' + ID + '_國軍醫院-台中.png')) :
             self.window.setStatusText(content="~條件符合，已截圖保存~",x=0.25,y=0.7,size=24)
         else:
             self.window.setStatusText(content="~不符合截圖標準~",x=0.3,y=0.7,size=24)
