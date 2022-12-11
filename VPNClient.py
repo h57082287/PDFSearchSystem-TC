9e1e87db67598451ffea7d4ed9a5537c4833ce43
CHUN_TING_WU <h57082287@gmail.com>
2022-12-11 15:21:14 +0800

    加入VPN啟用檢測

--- VPNClient.py
+++ VPNClient.py
@@ -22,33 +22,34 @@ class VPN():
         self.fileList = []
         self.index = 0
 
-        # 設定視窗
-        self.root = tk.Tk()
-        self.root.title("VPN設定視窗")
-        self.root.geometry("300x200")
-        self.root.resizable(width=False,height=False)
+        if (self.window.checkVal_AUVPNM):
+            # 設定視窗
+            self.root = tk.Tk()
+            self.root.title("VPN設定視窗")
+            self.root.geometry("300x200")
+            self.root.resizable(width=False,height=False)
 
-        # 設定文字
-        self.Text1 = tk.Label(self.root,text="OpenVPN帳號 : " ,font=("標楷體", 12))
-        self.Text1.place(relx=0.05,rely=0.2)
-        self.UserName = tk.Entry(self.root)
-        self.UserName.place(relx=0.45,rely=0.2)
+            # 設定文字
+            self.Text1 = tk.Label(self.root,text="OpenVPN帳號 : " ,font=("標楷體", 12))
+            self.Text1.place(relx=0.05,rely=0.2)
+            self.UserName = tk.Entry(self.root)
+            self.UserName.place(relx=0.45,rely=0.2)
 
-        # 設定文字
-        self.Text2 = tk.Label(self.root,text="OpenVPN密碼 : " ,font=("標楷體", 12))
-        self.Text2.place(relx=0.05,rely=0.35)
-        self.Password = tk.Entry(self.root,show='*')
-        self.Password.place(relx=0.45,rely=0.35)
+            # 設定文字
+            self.Text2 = tk.Label(self.root,text="OpenVPN密碼 : " ,font=("標楷體", 12))
+            self.Text2.place(relx=0.05,rely=0.35)
+            self.Password = tk.Entry(self.root,show='*')
+            self.Password.place(relx=0.45,rely=0.35)
 
-        # 建立按鈕
-        self.getPath = tk.Button(self.root,text="選擇.ovpn資料夾路徑",width=20,height=2,font=("標楷體",12),command=self._GetPath)
-        self.getPath.place(relx=0.25 , rely=0.5)
+            # 建立按鈕
+            self.getPath = tk.Button(self.root,text="選擇.ovpn資料夾路徑",width=20,height=2,font=("標楷體",12),command=self._GetPath)
+            self.getPath.place(relx=0.25 , rely=0.5)
 
-        # 建立按鈕
-        self.OKBTN = tk.Button(self.root,text="確認",width=7,height=1,font=("標楷體",12),command=self._OK,state="disable")
-        self.OKBTN.place(relx=0.37 , rely=0.78)
+            # 建立按鈕
+            self.OKBTN = tk.Button(self.root,text="確認",width=7,height=1,font=("標楷體",12),command=self._OK,state="disable")
+            self.OKBTN.place(relx=0.37 , rely=0.78)
 
-        self.root.mainloop()
+            self.root.mainloop()
         
     def _InstallationCkeck(self):
         cmd = 'start openvpn --config "C:\\Users\\h5708\\Downloads\\Surfshark_Config\\89.187.178.94_tcp.ovpn" --auth-user-pass login.conf'
