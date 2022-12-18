81af7fb4d2f7b55d0f42d08e571fabee72ba35a9
CHUN_TING_WU <h57082287@gmail.com>
2022-12-18 19:57:46 +0800

    修改VPN的啟動方式及亞洲大學的頁數筆數異常問題

--- auh.py
+++ auh.py
@@ -4,6 +4,9 @@ from PDFReader import PDFReader
 from bs4 import BeautifulSoup
 import os
 from LogController import Log
+from VPNClient import VPN
+from VPNWindow import VPNWindow
+from tkinter import messagebox
 
 # 亞洲大學
 class AUH():
@@ -20,46 +23,50 @@ class AUH():
         self.Data = []
         self.browser = browser
         # 各醫院新增項目
-        self.respone = None
-        self.ChangeIPNow = False
         self.idx = 0
         self.datalen = 0
         self.olddatalen = 0
         self.log = Log()
+        self.VPNSetup = 0 # 尚未初始化VPN狀態
+        if self.window.checkVal_AUVPNM :
+            self.VPN = VPN(self.window)
+            VPNWindow(self.VPN)
+            if not self.VPN.InstallationCkeck() :
+                messagebox.showerror("VPN異常","請檢查您是否有安裝OpenVPN !!!")
+                self.window.RunStatus = False
+                self.browser.quit()
+                os._exit(0)
+            self.VPNSetup = 1 # 啟用VPN模式
+            print(1234)
+        else:
+            self.VPNSetup = 2 # 未啟用VPN模式
 
     def run(self):
-        while True:
-            if self._PDFData() and self.window.RunStatus:
+        for currentPage in range(self.currentPage-1,self.EndPage):
+            if self._PDFData(currentPage) and self.window.RunStatus:
                 for self.idx in range(self.currentNum-1,self.datalen) :
-                    # 各醫院新增項目
-                    self._ChangingIPCK()
                     print(self.Data[self.idx])
-                    if (self.idx < self.EndNum) and (self.currentPage <= self.EndPage) and self.window.RunStatus:
-                        content = "姓名 : " + self.Data[self.idx]['Name'] + "\n身分證字號 : " + self.Data[self.idx]['ID'] + "\n出生日期 : " + self.Data[self.idx]['Born'] + "\n查詢醫院 : 亞洲大學附設醫院\n當前第" + str(self.currentPage) + "頁，第" + str(self.idx + 1) + "筆"
+                    if ((currentPage != self.EndPage -1) and (self.idx != self.EndNum - 1)) and self.window.RunStatus:
+                        content = "姓名 : " + self.Data[self.idx]['Name'] + "\n身分證字號 : " + self.Data[self.idx]['ID'] + "\n出生日期 : " + self.Data[self.idx]['Born'] + "\n查詢醫院 : 亞洲大學附設醫院\n當前第" + str(currentPage) + "頁，第" + str(self.idx + 1) + "筆"
                         self.window.setStatusText(content=content,x=0.3,y=0.75,size=12)
                         self._getReslut(self.Data[self.idx]['Name'], self.Data[self.idx]['ID'], self.Data[self.idx]['Born'].split('/')[0],self.Data[self.idx]['Born'].split('/')[1],self.Data[self.idx]['Born'].split('/')[2])
                         self._startBrowser(self.Data[self.idx]['Name'],self.Data[self.idx]['ID'])
-                        self.log.write(self.Data[self.idx]['Name'],self.Data[self.idx]['ID'],"亞洲大學附設醫院",self.Data[self.idx]['Born'],str(self.currentPage),str(self.idx + 1))
+                        self.log.write(self.Data[self.idx]['Name'],self.Data[self.idx]['ID'],"亞洲大學附設醫院",self.Data[self.idx]['Born'],str(currentPage),str(self.idx + 1))
                         time.sleep(2)
                     else:
                         break
-                    self.ChangeIPNow = True
-                self.olddatalen = self.datalen
-                self.currentPage += 1
             else:
-                self.window.setStatusText(content="~比對完成~",x=0.35,y=0.7,size=24)
-                # 各醫院新增項目 
-                self.window.RunStatus = False
-                time.sleep(2)
-                self.window.GUIRestart()
-                self._endBrowser()
                 break
+        self.window.setStatusText(content="~比對完成~",x=0.35,y=0.7,size=24)
+        time.sleep(2)
+        self.window.GUIRestart()
+        self._endBrowser()
         del self
 
     def _getReslut(self,name:str, ID:str, year:str, month:str, day:str):
-        self.respone = requests.get('https://appointment.auh.org.tw/cgi-bin/as/reg21.cgi?Tel=' + ID + '&sentbtn=%E7%A2%BA++++%E5%AE%9A&day=01&month=01&Year=088')
+        respone = requests.get('https://appointment.auh.org.tw/cgi-bin/as/reg21.cgi?Tel=' + ID + '&sentbtn=%E7%A2%BA++++%E5%AE%9A&day=01&month=01&Year=088')
         with open("reslut.html",'wb') as f :
-            f.write(self.respone.content)
+            f.write(respone.content)
 
     def _startBrowser(self,name,ID):
         self.browser.get(r'file:///' + os.path.dirname(os.path.abspath(__file__)) + '/reslut.html')
@@ -79,24 +86,15 @@ class AUH():
                 break
         return found
 
-    def _PDFData(self) -> bool:
+    def _PDFData(self,currentPage) -> bool:
         # print("Current : " + str(self.currentPage) + "  End : " + str(self.EndPage))
-        if (self.currentPage <= self.EndPage):
-            mPDFReader = PDFReader(self.window,self.filePath)
-            status, self.Data,self.datalen = mPDFReader.GetData(self.currentPage-1)
-            return status
-        else:
-            return False
+        mPDFReader = PDFReader(self.window,self.filePath)
+        status, self.Data,self.datalen = mPDFReader.GetData(currentPage)
+        return status
     
     def _endBrowser(self):
         self.browser.quit()
-    
-    # 各醫院新增項目
-    def _ChangingIPCK(self):
-        while(self.ChangeIPNow):
-            pass
-        self.ChangeIPNow = False
-
+        
     def __del__(self):
         print("物件刪除")
     
